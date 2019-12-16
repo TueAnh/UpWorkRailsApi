e30f9c14604b4c5ebe255371560631d9afcaf0c7
Tuan Anh <anhphanaabb@gmail.com>
2019-12-16 13:09:55 +0700

    convert timestamp to time, edit json for seeker_profiles

--- app/serializers/seeker_profile_serializer.rb
+++ app/serializers/seeker_profile_serializer.rb
@@ -1,10 +1,15 @@
+
 class SeekerProfileSerializer < ActiveModel::Serializer
   #giúp mình define lại file json 
   # rails g serializer <Model> 
-  attributes :id, :title, :user_id, :user_name
+  attributes :id, :title, :location, :user_id, :user_name, :created_at_convert
 
   def user_name
     object.user.name
+  end
+
+  def created_at_convert
+    Time.at(object.created_at).strftime("%B %e, %Y")
   end
 
 end
