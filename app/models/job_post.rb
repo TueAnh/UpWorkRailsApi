7364086821cc0ef1b20883a7de8406017554ee71
TueAnh <taubeopro@gmail.com>
2019-12-28 21:20:55 +0700

    fix model

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,8 +1,8 @@
 class JobPost < ApplicationRecord
-    has_many :job_post_activities,dependent: :destroy
-    has_many :users, through: :job_post_activities,dependent: :destroy
+    has_many :job_post_activities, dependent: :destroy, foreign_key: :job_post_id
+    has_many :users, through: :job_post_activities
 
-    has_many :job_post_skill_sets
+    has_many :job_post_skill_sets, dependent: :destroy, foreign_key: :job_post_id
     has_many :skill_sets, through: :job_post_skill_sets
     belongs_to :company
     belongs_to :job_location
--- app/models/job_post_skill_set.rb
+++ app/models/job_post_skill_set.rb
@@ -1,4 +1,4 @@
 class JobPostSkillSet < ApplicationRecord
     belongs_to :skill_set ,optional: true
-    belongs_to :job_post,optional: true, dependent: :destroy
+    belongs_to :job_post,optional: true
 end
